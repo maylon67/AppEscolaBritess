b6ad7df74690ac54d8ac9c923aaef0057b3e2884
Maylon <maylongatodemais25@gmail.com>
2022-08-18 11:38:52 -0300

    gjihjight

---
+++ android/app/src/main/kotlin/com/example/app_escola_bites/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_escola_bites
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ionicons/ionicons.dart';
+class LoginPage extends StatefulWidget {
+  const LoginPage({ Key? key }) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
