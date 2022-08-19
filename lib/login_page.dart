905bba81161b057d01d7d73af3c398b42ab27ed2
Maylon <maylongatodemais25@gmail.com>
2022-08-19 17:09:19 -0300

    ibjijti

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,7 +8,7 @@ class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
-
+TextEditingController emailControle = TextEditingController();
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
@@ -29,7 +29,29 @@ class _LoginPageState extends State<LoginPage> {
                       SizedBox(height: 40,),
                       Image.asset('lib/assets/escolaa.png',),
                       SizedBox(height: 8,),
-                      Text('Colégio Tiradentes',style: GoogleFonts.allura(color: Colors.black,fontSize: 29),)
+                      Text('Colégio Tiradentes',style: GoogleFonts.oswald(color: Color(0xFF083D99),fontSize: 29),),
+                      SizedBox(height: 60,),
+                      Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25),side:BorderSide(color: Color(0xff083d99),width: 2.5) ),
+
+                        child: TextFormField(
+                          controller: emailControle,
+                          decoration: InputDecoration(
+                             contentPadding: EdgeInsets.symmetric(vertical: 5),
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  errorBorder: InputBorder.none,
+                                  disabledBorder: InputBorder.none,
+                                  prefixIcon: Icon(Ionicons.mail_sharp,color: Color(0xFF083d99),),
+                                  labelText: 'Seu email',
+                                  labelStyle: GoogleFonts.oswald(
+                                    color: Color(0xff083d99),
+                                    fontSize: 20,
+                                  ),
+                          ),
+                        ),
+                      ),
                     ],
                 )
                 ),
