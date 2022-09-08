24e589e632f52c7a08d82d2a6572665265b8d2ac
Maylon <maylongatodemais25@gmail.com>
2022-09-08 17:01:47 -0300

    fhthjtht

---
+++ lib/services/querys/ws_get_saldo_caixa.dart
@@ -0,0 +1,21 @@
+import 'package:app_escola_bites/app_config.dart';
+import 'package:app_escola_bites/services/ws_config.dart';
+import 'package:flutter/material.dart';
+class wsSaldoCaixa{
+  Future<int> getSaldoCaixa(codigoCaixa,valorRifa) async{
+
+    try{
+      MapSD response = await WsController.executeWsPost(query: '/controller/getCaixa?cdcaixa=' + codigoCaixa, duration: Duration(seconds: 35));
+      
+      if (response.containsKey('error') || response.containsKey('connection') || response.isEmpty) return 0;
+      
+      String saldoCaixa = response['DTCAIXA'];
+      print(saldoCaixa);
+      
+      return int.parse(saldoCaixa);
+    } catch(e) {
+      print('===  ERROR  getAllCaixa : ${e.toString()} ===');
+      return 0;
+    }
+  }
+}
