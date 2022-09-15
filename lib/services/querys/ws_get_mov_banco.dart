734494c6a4a6c5b9947b7b0fa9c64474ad325186
Luis Fabiano <luidibaricheli000@gmail.com>
2022-09-15 10:50:32 -0300

    getmod

--- lib/services/querys/ws_get_mov_banco.dart
+++ lib/services/querys/ws_get_mov_banco.dart
@@ -11,7 +11,7 @@ class MovimentoBanco {
 
       String movimentoBanco = response[{
         "movimentos": [
-          {
+          { 
             "DATA": "31/08/2022",
             "OPERACAO": "MENSALIDADE",
             "VLRMOVBCO": "880",
