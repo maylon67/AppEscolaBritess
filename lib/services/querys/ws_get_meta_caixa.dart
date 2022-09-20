254c47ee76c771aef7d9dc879a00930fbe08c69d
Maylon <maylongatodemais25@gmail.com>
2022-09-20 08:24:20 -0300

    ngjbhhgbfg jk

--- lib/services/querys/ws_get_meta_caixa.dart
+++ lib/services/querys/ws_get_meta_caixa.dart
@@ -1,25 +1,26 @@
-/*import 'package:app_escola_bites/app_config.dart';
+import 'package:app_escola_bites/app_config.dart';
 import 'package:app_escola_bites/models/seu_saldo_models.dart';
 import 'package:app_escola_bites/services/querys/ws_meta_caixa.dart';
 import 'package:app_escola_bites/services/ws_config.dart';
 
 class WsMetaCaixa{
-  Future<MetaCaixa> getMetaCaixa(int numero) async{
+  WsMetaCaixa(String mensalidade, String rifa);
+
+  Future<String> getMetaCaixa(int numero) async{
 
     try{
       MapSD response = await WsController.executeWsPost(query: '/controller/getMetaCaixa?cdcaixa='+numero.toString(), duration: Duration(seconds: 35));
       
-      if (response.containsKey('error') || response.containsKey('connection') || response.isEmpty) return 0;
+      if (response.containsKey('error') || response.containsKey('connection') || response.isEmpty) return '';
       
       String mensalidade = response['VLRMENSALIDADE'];
       String rifa = response['VLRRIFA'];
-      MetaCaixa metaCaixa = MetaCaixa(mensalidade, rifa);
+      WsMetaCaixa metaCaixa = WsMetaCaixa(mensalidade, rifa);
 
-      return int.parse(metaCaixa);
+      return metaCaixa.toString();
     } catch(e) { 
       print('===  ERROR  getMetaCaixa : ${e.toString()} ===');
-      return 0;
+      return '';
     }
   }
 }
-*/
