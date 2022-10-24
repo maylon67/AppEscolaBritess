592b9b5f70865ee92b47c4aaf24f5c7d34542ce9
Luis Fabiano <luidibaricheli000@gmail.com>
2022-10-24 07:54:10 -0300

    align list

--- lib/tile_mov_caixa.dart
+++ lib/tile_mov_caixa.dart
@@ -1,5 +1,6 @@
 import 'package:app_escola_bites/models/movimento_caixa_models.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TileMovimentosCaixa extends StatefulWidget {
  // const TileMovCaixa({ Key? key }) : super(key: key);
@@ -13,11 +14,13 @@ class TileMovimentosCaixa extends StatefulWidget {
 }
 
 class _TileMovCaixaState extends State<TileMovimentosCaixa> {
+  static var f = NumberFormat("##,###,##0.00", "pt-br");
   @override
   Widget build(BuildContext context) {
     return Container(
      // color: Colors.transparent,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
             children: [
@@ -46,19 +49,66 @@ class _TileMovCaixaState extends State<TileMovimentosCaixa> {
           ),
           Column(
             children: [
-              Text(widget.movCaixa.operacao, style: TextStyle(),)
+              Text(widget.movCaixa.operacao, style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 16, 
+                  fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              ),
+              Divider(
+                color: Colors.black,
+                height: 0,
+                thickness: 1.5,
+              )
             ],
+          ),
+           Container(
+            height: 50,
+            child: VerticalDivider(
+              color: Colors.black,
+              thickness: 1.5,
+            ),
           ),
           Column(
             children: [
-              Text(widget.movCaixa.tipo)
+              Text(widget.movCaixa.tipo, style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 16, 
+                  fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              ),
+              Divider(
+                color: Colors.black,
+                height: 0,
+                thickness: 1.5,
+              )
             ],
+          ),
+           Container(
+            height: 50,
+            child: VerticalDivider(
+              color: Colors.black,
+              thickness: 1.5,
+            ),
           ),
           Column(
             children: [
-              Text(widget.movCaixa.valor.toString())
+              Text(f.format(widget.movCaixa.valor), style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 16, 
+                  fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.end,
+              ),
+              Divider(
+                color: Colors.black,
+                height: 0,
+                thickness: 1.5,
+              )
             ],
-          )
+          ),
         ],
       ),
     );
