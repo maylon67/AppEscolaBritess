2ffed0a76e1d083007bb46cd664cf5c526ff992e
Luis Fabiano <luidibaricheli000@gmail.com>
2022-11-03 10:34:33 -0300

    card

--- lib/tile_mov_banco.dart
+++ lib/tile_mov_banco.dart
@@ -1,3 +1,4 @@
+import 'package:app_escola_bites/app_config.dart';
 import 'package:app_escola_bites/models/movimento_models.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -15,95 +16,87 @@ class _TileMovBancoState extends State<TileMovBanco> {
   static var f = NumberFormat("##,###,##0.00", "pt-br");
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // color: Colors.transparent,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: 40,
-              child: Text(
-                widget.movBanco.data,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+    return InkWell(
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical:10, horizontal: 18),
+        shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(15)),
+        elevation: 6,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      padding: EdgeInsets.only(bottom: 7),
+                      width: 109,
+                      child: Column(
+                        children: [
+                                  Text('Data', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, color: Colors.black),),
+                                  Text(widget.movBanco.data.toString(), textAlign: TextAlign.center, style: TextStyle(fontSize: 16),),
+                        ]
+                                ),
+                    ),
+              Container(
+                height: 57,
+                child: VerticalDivider(
+                  color: corPadraoApp, thickness: 1,
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            Container(
-              height: 40,
-              child: VerticalDivider(
-                color: Colors.black,
-                thickness: 1,
-              ),
-            ),
-            Container(
-              height: 40,
-              width: 65,
-              child: Text(
-                widget.movBanco.operacao,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 7),
+                  child: Column(
+                    children: [
+                    Text('Operação', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, color: Colors.black,),),
+                    Text(widget.movBanco.operacao.toString(), textAlign: TextAlign.center, style: TextStyle(fontSize: 16),),
+                    ],
+                  ),
                 ),
-                textAlign: TextAlign.center,
+              )
+              
+                ]
               ),
-            ),
-            Container(
-              height: 40,
-              child: VerticalDivider(
-                color: Colors.black,
-                thickness: 1.5,
+              Divider(
+                height: 0,
+                color: corPadraoApp, thickness: 1,
               ),
-            ),
-            Container(
-              height: 40,
-              width: 20,
-              child: Text(
-                widget.movBanco.tipo,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Text('Tipo', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, color: Colors.black),),
+                          Text(widget.movBanco.tipo, textAlign: TextAlign.center, style: TextStyle(fontSize: 16),),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                height: 62,
+                child: VerticalDivider(
+                  color: corPadraoApp, thickness: 1,
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            Container(
-              height: 40,
-              child: VerticalDivider(
-                color: Colors.black,
-                thickness: 1.5,
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Text('Valor BCO', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, color: Colors.black),),
+                          Text(widget.movBanco.vlrmovbco.toString(), textAlign: TextAlign.center, style: TextStyle(fontSize: 16),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Container(
-              height: 30,
-              width: 75,
-              child: Text(
-                f.format(widget.movBanco.vlrmovbco),
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-          ],
+            ]),
         ),
-        Divider(
-          height: 1,
-          thickness: 1,
-          color: Colors.black,
-        )
-      ],
-    ));
+      ),
+    );
   }
 }
